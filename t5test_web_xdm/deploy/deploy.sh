313ad6bfb17102fdcfa4593c86d57732b9f46871
dawin <jinheung90@gmail.com>
2023-01-17 16:05:31 +0900

    workflow name 변경

--- t5test_web_xdm/deploy/deploy.sh
+++ t5test_web_xdm/deploy/deploy.sh
@@ -4,10 +4,10 @@ sudo chmod 775 -R /home/centos
 
 STDOUT=/home/centos/stdout.log
 STDERR=/home/centos/stderr.log
-
+DIR=/home/centos/deploy.jar
 sudo fuser -k 8081/tcp
 sudo sleep 20s # Waits  seconds.
-sudo nohup java -jar 1>>$STDOUT 2>> $STDERR &
+sudo nohup java -jar $DIR 1>>$STDOUT 2>> $STDERR &
 sudo sleep 60s
 
 exit 1
