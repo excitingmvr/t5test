b6c53745019b8f5d0544150bac3796d2ecd84f17
dawin <jinheung90@gmail.com>
2023-01-17 14:25:31 +0900

    test

--- t5test_web_xdm/deploy/deploy.sh
+++ t5test_web_xdm/deploy/deploy.sh
@@ -4,4 +4,7 @@ STDOUT=/home/centos/stdout.log
 STDERR=/home/centos/stderr.log
 
 sudo fuser -k 8081/tcp
+sudo sleep 20s # Waits  seconds.
 sudo nohup java -jar 1>>$STDOUT 2>> $STDERR &
+sudo sleep 60s
+exit 1
