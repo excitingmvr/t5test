0db073f0e5420a4a459d3b137b8c811ac85b2533
dawin <jinheung90@gmail.com>
2023-01-27 16:49:34 +0900

    script 수정

--- t5test_web_xdm/deploy/deploy.sh
+++ t5test_web_xdm/deploy/deploy.sh
@@ -2,12 +2,19 @@
 
 sudo chmod 775 -R /home/centos
 
-STDOUT=/home/centos/stdout.log
-STDERR=/home/centos/stderr.log
-DIR=/home/centos/deploy.jar
+STDOUT=/home/centos/deploy/stdout.log
+STDERR=/home/centos/deploy/stderr.log
+DIR=/home/centos/deploy/deploy.jar
+
 sudo fuser -k 8081/tcp
 sudo sleep 20s # Waits  seconds.
-sudo nohup java -jar $DIR 1>>$STDOUT 2>> $STDERR &
+sudo nohup java -jar Dserver.port=8081 $DIR 1>>$STDOUT 2>> $STDERR &
+sudo sleep 60s
+
+sudo fuser -k 8082/tcp
+sudo sleep 20s # Waits  seconds.
+sudo nohup java -jar Dserver.port=8082 $DIR 1>>$STDOUT 2>> $STDERR &
 sudo sleep 60s
 
 exit 0
+
